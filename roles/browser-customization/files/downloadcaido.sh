e573fd4062ba61ac837cfc3d79160bf39b040274
perumal-j <perumalnetworkeng@gmail.com>
2025-06-20 13:25:38 +0530

    adfv

---
+++ roles/browser-customization/files/downloadcaido.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+	ARCH="x86_64"
+elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+	ARCH="AArch64"
+else
+	echo "Unsupported Architecture: $ARCH"
+	exit 1
+fi
+
+jq -r '.body' /tmp/caido-latest.json | grep -oP 'https://[^"]+\.AppImage' | grep $ARCH
